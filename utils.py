1b8ce343c8f53cf1f616d6ac1839e97fe0c98b33
tnn1t1s <david.j.palaitis@gmail.com>
2021-05-03 12:51:05 -0400

    cleanup docs

--- utils.py
+++ utils.py
@@ -1,34 +1,50 @@
+"""Tools for working with Cryptopunk NFTs; this includes utilities for data analysis and image preparation for training machine learning models using Cryptopunks as training data. 
 
-__DATA_DIR__ = "/home/david/artwork/zombiepunx/data"
-__PUNK_DIR__ = f"{__DATA_DIR__}/punx/images/training";
+Functions:
+
+    get_punk(id)
+    pixel_to_img(pixel_str, dim)
+    flatten(img)
+    unflatten(img)
+    sort_dict_by_function_of_value(d, f)
+    add_index_to_colors(colors)
+
+"""
+import os
+
+__ROOT_DIR__ = os.path.dirname(os.path.abspath(__file__))
+__PUNK_DIR__ = f"{__ROOT_DIR__}/data/punx/images/training";
 
 def get_punk(id):
-    """ returns a ndarray with loaded image
-    """
-    return mpimg.imread(f"""{__PUNK_DIR__}/punk{"%04d" % id}.png""")
+    '''
+       Returns a ndarray with loaded image
+    ''' 
+    return mpimg.imread(f'''{__PUNK_DIR__}/punk{"%04d" % id}.png''')
 
-def pixel_to_img(key, dim = (24,24)):
-    """take pixel of format
-       ""[0.37254903 0.11372549 0.03529412 1.0]"
+def pixel_to_img(pixel_str, dim = (24,24)):
+    '''
+       Take pixel of format "[r,g,b,b]"
        and return an image of size `dim` containing 
        only the pixel's color.
-    """
+    '''
     (x,y) = dim 
-    c = np.fromstring(key[1:-1], float, sep=' ')
+    c = np.fromstring(pixel_str[1:-1], float, sep=' ')
     return np.full((x, y, 4), c)
 
 def flatten(img): 
-  """convert (x,y,z) array containing a pixel in z-dimension 
+  '''
+     Convert (x,y,z) array containing a pixel in z-dimension 
      to an (x,y) array with str values for each (i,j)
      the intention is to make this easier to work with in ML
      training.
-  """
+  '''
   return np.array([[str(c) for c in row] 
                    for row in img])
 
 def unflatten(img):
-  """return a flattend image to valid .png format for display
-  """  
+  '''
+     Return a flattend image to valid .png format for display
+  '''  
   return np.array([[np.fromstring(c[1:-1], float, sep=' ') 
                     for c in row] for row in img])
 
@@ -38,10 +54,11 @@ def sort_dict_by_function_of_value(d, f = len):
   return {k: v for k, v in sorted_tuples}
 
 def add_index_to_colors(colors):
-  """add a unique, sequential index to the entry for 
+  '''
+     Add a unique, sequential index to the entry for 
      each color. returned dictionary will be of form
      {`color_string`: { `"id": `int`, "punkIds" : `list[int`}}
-  """
+  '''
   i=0
   d={}
   for k in colors.keys():
