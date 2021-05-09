e5edf7eceb9498573d3e397ff7c3cd5ca5a3476a
tnn1t1s <david.j.palaitis@gmail.com>
2021-05-09 17:10:05 -0400

    attribute tagging for all punks

--- utils.py
+++ utils.py
@@ -11,6 +11,8 @@ Functions:
 
 """
 import os
+import requests
+from collections import OrderedDict
 
 __ROOT_DIR__ = os.path.dirname(os.path.abspath(__file__))
 __PUNK_DIR__ = f"{__ROOT_DIR__}/data/punx/images/training";
@@ -68,5 +70,78 @@ def add_index_to_colors(colors):
     }
     i=i+1
   return d
+
+def get_attr_dict():
+    '''
+       Read the attr csv and populate a default dict
+    '''
+    d=OrderedDict()
+    with open(f"{__ROOT_DIR__}/data/punx/list_attr_punx.csv") as f:
+        for attr in f.read().split(','):
+            d[attr]=-1
+    return d
+
+__ATTR_DICT__  = get_attr_dict()
+
+def get_punk_attrs(id):
+    '''
+       Retrieve `id` cryptopunk from larvalabs.com,
+       parse HTML to extract type and attribute list
+       to return list of attributes
+    '''
+    typeClass="col-md-10 col-md-offset-1 col-xs-12"
+    punk_html=requests.get(f"https://www.larvalabs.com/cryptopunks/details/{id}").text
+    soup = BeautifulSoup(punk_html, 'html.parser')
+    details = soup.find(id="punkDetails")
+
+    punkType = camelCase(details.find(class_=typeClass).find('a').contents[0])
+
+    attrs=[punkType]
+    attrTags = details.find(class_ = "row detail-row")
+    for attrTag in attrTags.find_all('a'):
+        attrs.append(camelCase(attrTag.contents[0]))
+    return attrs
+
+def deepcopy(d):
+    return {k:d[k] for k in d} 
+
+def get_punk_dict(id):
+   '''
+      Retrieve a punk pages, pull type and attributes
+      from HTML and return a dictionary of attribute to
+      (-1,1) mapping where 1 is truthy for existence of 
+      attribute
+   '''
+   od = {k:_ATTR_DICT[k] for k in __ATTR_DICT__} 
+   attrs = get_punk_attrs(id)
+   for attr in attrs:
+      od[attr]=1
+   return od
+
+def get_punks(start, end):
+    '''
+      Retrieve punks in range `start` to `end`
+    '''
+    punks={}
+    for id in range(start, end):
+        punks[id] = get_punk_dict(id)
+    return punks
+
+def punks_to_csv(punks):
+    '''
+       Write `punks` to a csv
+    '''
+    return None
+
+def punks_from_csv(fp):
+    '''
+       Read punks from a csv
+    '''
+    return {}
+
+
+
+
+
 
 
